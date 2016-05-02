d6fb5c7dfa90bb09a38aa6e077cac462ee12bf28
Bruno Baldochi <brunoyb@gmail.com>
2016-05-01 21:33:14 -0300

    p4merge: Update to the latest version
    The package was completely rewritten.

---
+++ packages/p4merge/tools/chocolateyUninstall.ps1
@@ -0,0 +1,22 @@
+﻿$packageName = 'p4merge'
+$fileType = 'msi'
+$silentArgs = '/qn'
+
+$local_key = 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*'
+$machine_key = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*'
+$machine_key6432 = 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*'
+
+$softwareName = 'Helix Visual Components'
+
+[array] $key = Get-ItemProperty -Path @($machine_key6432, $machine_key, $local_key) `
+                                -ErrorAction SilentlyContinue | ? { $_.DisplayName -Like $softwareName }
+
+if ($key.Count -eq 1) {
+	$key | % {
+		$silentArgs = "$($_.PSChildName) $silentArgs"
+
+		Uninstall-ChocolateyPackage -PackageName $packageName `
+		                            -FileType $fileType `
+		                            -SilentArgs $silentArgs
+	}
+}
