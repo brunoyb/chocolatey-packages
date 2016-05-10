f6e9cfe7b328aa55358cb8dc8adfded2afdcf1eb
Bruno Baldochi <brunoyb@gmail.com>
2016-05-10 03:34:05 -0300

    mirc: Add 0 as a valid exit code

--- packages/mirc/tools/chocolateyInstall.ps1
+++ packages/mirc/tools/chocolateyInstall.ps1
@@ -4,7 +4,7 @@ $silentArgs = '/S /NOBACKUP'
 $url = 'http://www.mirc.com/get.php?version=745'
 $checksum = '8d8c2949756dd619c9bbb5ade7a04bc24d2ee40b'
 $checksumType = 'sha1'
-$validExitCodes = @(1223)
+$validExitCodes = @(0, 1223)
 
 Install-ChocolateyPackage -PackageName $packageName `
                           -FileType $fileType `
