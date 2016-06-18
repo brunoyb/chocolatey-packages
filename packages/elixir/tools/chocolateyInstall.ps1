5ccc2319305ed853d9fd012477525100b2feb1db
Bruno Baldochi <brunoyb@gmail.com>
2016-06-18 20:57:13 -0300

    elixir: Update to v1.2.6

--- packages/elixir/tools/chocolateyInstall.ps1
+++ packages/elixir/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = 'elixir'
-$url = 'https://github.com/elixir-lang/elixir/releases/download/v1.2.5/Precompiled.zip'
-$checksum = '4bf8221e6776aec0e1fb58cbaef20327c9a1089c'
+$url = 'https://github.com/elixir-lang/elixir/releases/download/v1.2.6/Precompiled.zip'
+$checksum = 'fb1cee26875399ef38b173049d16f0028bf3add7'
 $checksumType = 'sha1'
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
@@ -10,4 +10,4 @@ Install-ChocolateyZipPackage -PackageName $packageName `
                              -ChecksumType $checksumType `
                              -UnzipLocation $toolsDir
 
-Install-ChocolateyPath "$toolsDir\bin" 'Machine'
+Install-ChocolateyPath "$(Join-Path $toolsDir 'bin')" 'Machine'
