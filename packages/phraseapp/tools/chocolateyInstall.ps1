44f689cbb36e64e52b99dc6a98227fe649944e47
Bruno Baldochi <brunoyb@gmail.com>
2016-06-18 23:11:33 -0300

    phraseapp: Add package

---
+++ packages/phraseapp/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿$packageName = 'phraseapp'
+$url = 'https://github.com/phrase/phraseapp-client/releases/download/1.1.17/phraseapp_windows_amd64.exe.zip'
+$checksum = '73cdb1e3bdfff3ee40251c216d1879c4dee72473'
+$checksumType = 'sha1'
+$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName $packageName `
+                             -Url $url `
+                             -Checksum $checksum `
+                             -ChecksumType $checksumType `
+                             -UnzipLocation $toolsDir
+
+Move-Item "$(Join-Path $toolsDir 'phraseapp_windows_amd64.exe')" "$(Join-Path $toolsDir 'phraseapp.exe')" -Force
