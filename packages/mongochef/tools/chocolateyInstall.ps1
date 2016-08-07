b8e5e0364da05da86c705dec39cafafe8ef6e874
Bruno Baldochi <brunoyb@gmail.com>
2016-08-07 00:00:10 -0300

    mongochef: Update to v4.1.0

--- packages/mongochef/tools/chocolateyInstall.ps1
+++ packages/mongochef/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
 ﻿$packageName = 'mongochef'
-$url = 'https://cdn.3t.io/mongochef-core/windows/4.0.4/mongochef-x86.msi.zip'
-$checksum = 'ab71061ad9efb65159b9212ca648156436254c74'
+$url = 'https://cdn.3t.io/mongochef-core/windows/4.1.0/mongochef-x86.msi.zip'
+$checksum = 'd5c5696110b8707df02b07a779799b7b5db5b442'
 $checksumType = 'sha1'
-$url64 = 'https://cdn.3t.io/mongochef-core/windows/4.0.4/mongochef-x64.msi.zip'
-$checksum64 = '1763db842283083fe385d43909b48afafcf6318d'
+$url64 = 'https://cdn.3t.io/mongochef-core/windows/4.1.0/mongochef-x64.msi.zip'
+$checksum64 = 'be00e159523cc9db45a3f999d41f259910ad1049'
 $checksumType64 = 'sha1'
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
