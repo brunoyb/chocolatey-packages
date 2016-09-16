505887879cbe9adabae29df8acedff0eaac66b29
Bruno Baldochi <brunoyb@gmail.com>
2016-09-16 17:17:47 -0300

    haskell-stack: Update to v1.2.0

--- packages/haskell-stack/tools/chocolateyInstall.ps1
+++ packages/haskell-stack/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$packageName = 'haskell-stack'
-$url = 'https://github.com/commercialhaskell/stack/releases/download/v1.1.2/stack-1.1.2-windows-i386.zip'
-$checksum = 'a68667c9d64fb65d8db5e00f3f8d0036e3b37a26'
-$checksumType = 'sha1'
-$url64 = 'https://github.com/commercialhaskell/stack/releases/download/v1.1.2/stack-1.1.2-windows-x86_64.zip'
-$checksum64 = 'ff0d12a7b7db5d5e398585b7a0c4d35b43a06013'
-$checksumType64 = 'sha1'
+$url = 'https://github.com/commercialhaskell/stack/releases/download/v1.2.0/stack-1.2.0-windows-i386.zip'
+$checksum = 'c2517b9210b023446fca44321fc5347db30a3b9bec2faf19e078069d161c13c6'
+$checksumType = 'sha256'
+$url64 = 'https://github.com/commercialhaskell/stack/releases/download/v1.2.0/stack-1.2.0-windows-x86_64.zip'
+$checksum64 = 'aa30b757777f6e1b888250a91c8ba986d60b9dfe094f1aa408bd7e90fa92cc8c'
+$checksumType64 = 'sha256'
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
 Install-ChocolateyZipPackage -PackageName $packageName `
