f723d9d08b8ccfedaead196aaf4aeda1a9ea3031
Bruno Baldochi <brunoyb@gmail.com>
2016-12-26 22:18:24 -0200

    phraseapp: Update to v1.3.0

--- packages/phraseapp/tools/chocolateyInstall.ps1
+++ packages/phraseapp/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = 'phraseapp'
-$url = 'https://github.com/phrase/phraseapp-client/releases/download/1.2.3/phraseapp_windows_amd64.exe.zip'
-$checksum = 'c5d5672350da4cb503ab2318cac2dfb467133f12fd158f157bf88b9e62427125'
+$url = 'https://github.com/phrase/phraseapp-client/releases/download/1.3.0/phraseapp_windows_amd64.exe.zip'
+$checksum = 'b96720dcf710ed4e820e7e9fb1227b8a732bd007f98438eb81d17a9ec45c6d69'
 $checksumType = 'sha256'
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
