2fcae39b82262c07de1d0d0004ef5e47ef326cd6
Bruno Baldochi <brunoyb@gmail.com>
2017-01-06 23:19:18 -0200

    haskell-stack: Add uninstall script
    This is to remove the added directory from the Path environment
    variable. I'm using the Uninstall-ChocolateyPath helper which hopefully
    will be available on Chocolatey soon.

---
+++ packages/haskell-stack/tools/chocolateyUninstall.ps1
@@ -0,0 +1,5 @@
+﻿$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+
+. $toolsDir\Uninstall-ChocolateyPath.ps1
+
+Uninstall-ChocolateyPath "$(Join-Path $env:APPDATA 'local\bin')"
