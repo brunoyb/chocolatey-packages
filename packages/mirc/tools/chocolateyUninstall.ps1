49514c0d9a1c77b7e9d5f959bc16d7b73228a79c
Bruno Baldochi <brunoyb@gmail.com>
2017-03-18 03:06:31 -0300

    mirc: Simplify uninstall script
    By using Get-UninstallRegistryKey from chocolatey-core.extension we
    could simplify the uninstall script by a lot.

--- packages/mirc/tools/chocolateyUninstall.ps1
+++ packages/mirc/tools/chocolateyUninstall.ps1
@@ -2,17 +2,10 @@
 $fileType = 'exe'
 $silentArgs = '/S'
 
-$local_key = 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*'
-$machine_key = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*'
-$machine_key6432 = 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*'
-
-$softwareName = 'mIRC'
-
-[array] $key = Get-ItemProperty -Path @($machine_key6432, $machine_key, $local_key) `
-                                -ErrorAction SilentlyContinue | ? { $_.DisplayName -Like $softwareName }
+[array] $key = Get-UninstallRegistryKey -SoftwareName 'mIRC'
 
 if ($key.Count -eq 1) {
-	$key | % {
+	$key | ForEach-Object {
 		$splitPosition = $_.UninstallString.IndexOf('_?=')
 
 		$file = $_.UninstallString.Substring(0, $splitPosition - 1)
