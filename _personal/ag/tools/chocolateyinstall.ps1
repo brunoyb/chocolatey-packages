22ed476378206a75525d0d452dff6318c46b52c4
Bruno Baldochi <brunoyb@gmail.com>
2017-06-10 00:58:45 -0300

    ag: Add package

---
+++ _personal/ag/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName = 'ag'
+$url = 'https://github.com/JFLarvoire/the_silver_searcher/releases/download/1.0.2-Windows/ag.zip'
+$checksum = 'ee336ba4afdaf3a4b3c350783dcf30a1f53cd1d192428082df11bf7ee3e168c6'
+$checksumType = 'sha256'
+$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName $packageName `
+                             -Url $url `
+                             -Checksum $checksum `
+                             -ChecksumType $checksumType `
+                             -UnzipLocation $toolsDir
+
+if ((Get-ProcessorBits 64) -And $env:chocolateyForceX86 -NE $true) {
+	New-Item "$(Join-Path $toolsDir 'WIN32\ag.exe.ignore')" -Type file -Force | Out-Null
+} else {
+	New-Item "$(Join-Path $toolsDir 'WIN64\ag.exe.ignore')" -Type file -Force | Out-Null
+}
---
+++ _personal/ag/update.ps1
@@ -0,0 +1,29 @@
+﻿Import-Module AU
+
+$releases = 'https://github.com/JFLarvoire/the_silver_searcher/releases'
+
+function global:au_SearchReplace {
+	@{
+		'tools\chocolateyInstall.ps1' = @{
+			"(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL)'"
+			"(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+		}
+	}
+}
+
+function global:au_GetLatest {
+	$downloadPage = Invoke-WebRequest -Uri $releases
+
+	$re = 'ag.zip$'
+	$url = $downloadPage.Links | Where-Object href -Match $re | Select-Object -First 1 -Expand href
+
+	$version = $url -Split '/|-' | Select-Object -Last 1 -Skip 2
+	$url = 'https://github.com' + $url
+
+	@{
+		Version = $version
+		URL = $url
+	}
+}
+
+Update-Package -ChecksumFor 32
