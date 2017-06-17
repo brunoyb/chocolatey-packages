5fdd6c74a23b273d388261a36877bf9ad9d3794f
Bruno Baldochi <brunoyb@gmail.com>
2017-06-17 16:38:23 -0300

    robo3t.install: Add package

---
+++ robo3t.install/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName = 'robo3t.install'
+$fileType = 'exe'
+$silentArgs = '/S'
+$url64 = 'https://download.robomongo.org/1.1.1/windows/robo3t-1.1.1-windows-x86_64-c93c6b0.exe'
+$checksum64 = 'fc871b31f40d758aab40d29b56341f670940ef39de6f08a5cca7325480091c90'
+$checksumType64 = 'sha256'
+
+Install-ChocolateyPackage -PackageName $packageName `
+                          -FileType $fileType `
+                          -SilentArgs $silentArgs `
+                          -Url64bit $url64 `
+                          -Checksum64 $checksum64 `
+                          -ChecksumType64 $checksumType64
---
+++ robo3t.install/update.ps1
@@ -0,0 +1,33 @@
+﻿Import-Module AU
+
+$download = 'https://robomongo.org/download'
+
+function global:au_SearchReplace {
+	@{
+		".\tools\chocolateyInstall.ps1" = @{
+			"(?i)(^\s*[$]packageName\s*=\s*)('.*')"    = "`$1'$($Latest.PackageName)'"
+			"(?i)(^\s*[$]fileType\s*=\s*)('.*')"       = "`$1'$($Latest.FileType)'"
+			"(?i)(^\s*[$]url64\s*=\s*)('.*')"          = "`$1'$($Latest.URL64)'"
+			"(?i)(^\s*[$]checksum64\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum64)'"
+			"(?i)(^\s*[$]checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
+		}
+	}
+}
+
+function global:au_GetLatest {
+	$downloadPage = Invoke-WebRequest -Uri $download
+
+	$re = 'robo3t-.+-windows-x86_64-.+\.exe$'
+	$url64 = $downloadPage.Links | Where-Object href -Match $re | Select-Object -First 1 -Expand href
+
+	$version = $url64 -Split '-' | Select-Object -First 1 -Skip 1
+
+	@{
+		Version = $version
+		URL64 = $url64
+	}
+}
+
+if ($MyInvocation.InvocationName -NE '.') {
+	Update-Package -ChecksumFor 64
+}
