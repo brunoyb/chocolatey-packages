e4363ea13faae0d9eeb48556729afdc09b229b86
Bruno Baldochi <brunoyb@gmail.com>
2017-06-17 21:47:36 -0300

    elixir: Small adjustments

--- _personal/elixir/tools/chocolateyInstall.ps1
+++ _personal/elixir/tools/chocolateyInstall.ps1
@@ -1,4 +1,6 @@
-﻿$packageName = 'elixir'
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName = 'elixir'
 $url = 'https://github.com/elixir-lang/elixir/releases/download/v1.4.4/Precompiled.zip'
 $checksum = '3fc2cc2ec39315d9894a81b9d167029e4a9cfa5bb22edb3d7e0e66971d4e43ed'
 $checksumType = 'sha256'
--- _personal/elixir/update.ps1
+++ _personal/elixir/update.ps1
@@ -1,12 +1,18 @@
-Import-Module AU
+﻿Import-Module AU
 
 $releases = 'https://github.com/elixir-lang/elixir/releases'
 
 function global:au_SearchReplace {
 	@{
-		'tools\chocolateyInstall.ps1' = @{
-			"(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL)'"
-			"(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+		".\tools\chocolateyInstall.ps1" = @{
+			"(?i)(^\s*[$]packageName\s*=\s*)('.*')"  = "`$1'$($Latest.PackageName)'"
+			"(?i)(^\s*[$]url\s*=\s*)('.*')"          = "`$1'$($Latest.URL)'"
+			"(?i)(^\s*[$]checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
+			"(?i)(^\s*[$]checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+		}
+
+		"$($Latest.PackageName).nuspec" = @{
+			"(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`$1$($Latest.ReleaseNotes)`$2"
 		}
 	}
 }
@@ -14,15 +20,17 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
 	$downloadPage = Invoke-WebRequest -Uri $releases
 
-	$re = 'Precompiled.zip$'
+	$re = 'Precompiled\.zip$'
 	$url = $downloadPage.Links | Where-Object href -Match $re | Select-Object -First 1 -Expand href
 
 	$version = $url -Split '/|v' | Select-Object -Last 1 -Skip 1
 	$url = 'https://github.com' + $url
+	$releaseNotes = "https://github.com/elixir-lang/elixir/releases/tag/v${version}"
 
 	@{
 		Version = $version
 		URL = $url
+		ReleaseNotes = $releaseNotes
 	}
 }
 
