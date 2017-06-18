0f5a35e24ab1431212726684eee5146e6193ed36
Bruno Baldochi <brunoyb@gmail.com>
2017-06-17 22:26:31 -0300

    phraseapp: Small adjustments

--- _personal/phraseapp/tools/chocolateyInstall.ps1
+++ _personal/phraseapp/tools/chocolateyInstall.ps1
@@ -1,13 +1,15 @@
-﻿$packageName = 'phraseapp-x64'
-$url = 'https://github.com/phrase/phraseapp-client/releases/download/1.4.5/phraseapp_windows_amd64.exe.zip'
-$checksum = 'c320c6cd35e368462d928ed08468d8c5e667f55b8f8fd754d39e91e22a3fbdc2'
-$checksumType = 'sha256'
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName = 'phraseapp'
+$url64 = 'https://github.com/phrase/phraseapp-client/releases/download/1.4.5/phraseapp_windows_amd64.exe.zip'
+$checksum64 = 'c320c6cd35e368462d928ed08468d8c5e667f55b8f8fd754d39e91e22a3fbdc2'
+$checksumType64 = 'sha256'
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
 Install-ChocolateyZipPackage -PackageName $packageName `
-                             -Url $url `
-                             -Checksum $checksum `
-                             -ChecksumType $checksumType `
+                             -Url64bit $url64 `
+                             -Checksum64 $checksum64 `
+                             -ChecksumType64 $checksumType64 `
                              -UnzipLocation $toolsDir
 
 Move-Item -Force "$(Join-Path $toolsDir 'phraseapp_windows_amd64.exe')" "$(Join-Path $toolsDir 'phraseapp.exe')"
--- _personal/phraseapp/update.ps1
+++ _personal/phraseapp/update.ps1
@@ -1,12 +1,18 @@
-Import-Module AU
+﻿Import-Module AU
 
 $releases = 'https://github.com/phrase/phraseapp-client/releases'
 
 function global:au_SearchReplace {
 	@{
-		'tools\chocolateyInstall.ps1' = @{
-			"(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL)'"
-			"(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+		".\tools\chocolateyInstall.ps1" = @{
+			"(?i)(^\s*[$]packageName\s*=\s*)('.*')"    = "`$1'$($Latest.PackageName)'"
+			"(?i)(^\s*[$]url64\s*=\s*)('.*')"          = "`$1'$($Latest.URL64)'"
+			"(?i)(^\s*[$]checksum64\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum64)'"
+			"(?i)(^\s*[$]checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
+		}
+
+		"$($Latest.PackageName).nuspec" = @{
+			"(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`$1$($Latest.ReleaseNotes)`$2"
 		}
 	}
 }
@@ -14,15 +20,17 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
 	$downloadPage = Invoke-WebRequest -Uri $releases
 
-	$re = 'phraseapp_windows_amd64.exe.zip$'
+	$re = 'phraseapp_windows_amd64\.exe\.zip$'
 	$url = $downloadPage.Links | Where-Object href -Match $re | Select-Object -First 1 -Expand href
 
 	$version = $url -Split '/' | Select-Object -Last 1 -Skip 1
 	$url = 'https://github.com' + $url
+	$releaseNotes = "https://github.com/phrase/phraseapp-client/releases/tag/${version}"
 
 	@{
 		Version = $version
-		URL = $url
+		URL64 = $url
+		ReleaseNotes = $releaseNotes
 	}
 }
 
