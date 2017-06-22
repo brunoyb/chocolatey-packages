511af63e06c58cafc69929de3582444f4d495024
Bruno Baldochi <brunoyb@gmail.com>
2017-06-21 22:22:14 -0300

    ripgrep: Add package

---
+++ _personal/ripgrep/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName = 'ripgrep'
+$url32 = 'https://github.com/BurntSushi/ripgrep/releases/download/0.5.2/ripgrep-0.5.2-i686-pc-windows-msvc.zip'
+$checksum32 = 'd9c0c7bfb2dd8203be8a56c8bea1eb704ac7f2e3e813f590cc8551c1d142d43e'
+$checksumType32 = 'sha256'
+$url64 = 'https://github.com/BurntSushi/ripgrep/releases/download/0.5.2/ripgrep-0.5.2-x86_64-pc-windows-msvc.zip'
+$checksum64 = '377e76c4877aeef3419438b95f7a9b08060a009aec96cc2cb7707a5518a7fa98'
+$checksumType64 = 'sha256'
+$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName $packageName `
+                             -Url $url32 `
+                             -Checksum $checksum32 `
+                             -ChecksumType $checksumType32 `
+                             -Url64bit $url64 `
+                             -Checksum64 $checksum64 `
+                             -ChecksumType64 $checksumType64 `
+                             -UnzipLocation $toolsDir
---
+++ _personal/ripgrep/update.ps1
@@ -0,0 +1,42 @@
+﻿Import-Module AU
+
+$releases = 'https://github.com/BurntSushi/ripgrep/releases'
+
+function global:au_SearchReplace {
+	@{
+		".\tools\chocolateyInstall.ps1" = @{
+			"(?i)(^\s*[$]packageName\s*=\s*)('.*')"    = "`$1'$($Latest.PackageName)'"
+			"(?i)(^\s*[$]url32\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
+			"(?i)(^\s*[$]checksum32\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
+			"(?i)(^\s*[$]checksumType32\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+			"(?i)(^\s*[$]url64\s*=\s*)('.*')"          = "`$1'$($Latest.URL64)'"
+			"(?i)(^\s*[$]checksum64\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum64)'"
+			"(?i)(^\s*[$]checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
+		}
+
+		"$($Latest.PackageName).nuspec" = @{
+			"(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`$1$($Latest.ReleaseNotes)`$2"
+		}
+	}
+}
+
+function global:au_GetLatest {
+	$downloadPage = Invoke-WebRequest -Uri $releases
+
+	$re = 'ripgrep-.+-.+-pc-windows-msvc\.zip$'
+	$url = $downloadPage.Links | Where-Object href -Match $re | Select-Object -First 2 -Expand href
+
+	$version = $url[0] -Split '-' | Select-Object -Last 1 -Skip 4
+	$url32 = 'https://github.com' + $url[0]
+	$url64 = 'https://github.com' + $url[1]
+	$releaseNotes = "https://github.com/BurntSushi/ripgrep/releases/tag/${version}"
+
+	@{
+		Version = $version
+		URL32 = $url32
+		URL64 = $url64
+		ReleaseNotes = $releaseNotes
+	}
+}
+
+Update-Package
