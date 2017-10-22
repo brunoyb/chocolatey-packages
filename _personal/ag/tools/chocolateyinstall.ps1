682541cfd39eea127ec6ea406fc7cfc91d2b9996
Bruno Baldochi <brunoyb@gmail.com>
2017-10-22 21:13:01 -0200

    ag: Update to v2.1.0.1
    Also change to another unofficial build.

--- _personal/ag/tools/chocolateyinstall.ps1
+++ _personal/ag/tools/chocolateyinstall.ps1
@@ -1,19 +1,19 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'ag'
-$url = 'https://github.com/JFLarvoire/the_silver_searcher/releases/download/1.0.2-Windows/ag.zip'
-$checksum = 'ee336ba4afdaf3a4b3c350783dcf30a1f53cd1d192428082df11bf7ee3e168c6'
-$checksumType = 'sha256'
+$url32 = 'https://github.com/k-takata/the_silver_searcher-win32/releases/download/2017-08-31/2.1.0-1/ag-2017-08-31_2.1.0-1-x86.zip'
+$checksum32 = '28456a424a30b12be3be2b9d427bc67031bc436f87f1100607cfa9a44c1af95d'
+$checksumType32 = 'sha256'
+$url64 = 'https://github.com/k-takata/the_silver_searcher-win32/releases/download/2017-08-31/2.1.0-1/ag-2017-08-31_2.1.0-1-x64.zip'
+$checksum64 = '2f0b83e705d6224df82be92014726875bec925c6b56ef84170d1f19eddfaa439'
+$checksumType64 = 'sha256'
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
 Install-ChocolateyZipPackage -PackageName $packageName `
-                             -Url $url `
-                             -Checksum $checksum `
-                             -ChecksumType $checksumType `
+                             -Url $url32 `
+                             -Checksum $checksum32 `
+                             -ChecksumType $checksumType32 `
+                             -Url64bit $url64 `
+                             -Checksum64 $checksum64 `
+                             -ChecksumType64 $checksumType64 `
                              -UnzipLocation $toolsDir
-
-if ((Get-ProcessorBits 64) -And $env:chocolateyForceX86 -NE $true) {
-	New-Item "$(Join-Path $toolsDir 'WIN32\ag.exe.ignore')" -Type file -Force | Out-Null
-} else {
-	New-Item "$(Join-Path $toolsDir 'WIN64\ag.exe.ignore')" -Type file -Force | Out-Null
-}
--- _personal/ag/update.ps1
+++ _personal/ag/update.ps1
@@ -1,14 +1,17 @@
 ﻿Import-Module AU
 
-$releases = 'https://github.com/JFLarvoire/the_silver_searcher/releases'
+$releases = 'https://github.com/k-takata/the_silver_searcher-win32/releases'
 
 function global:au_SearchReplace {
 	@{
 		".\tools\chocolateyInstall.ps1" = @{
-			"(?i)(^\s*[$]packageName\s*=\s*)('.*')"  = "`$1'$($Latest.PackageName)'"
-			"(?i)(^\s*[$]url\s*=\s*)('.*')"          = "`$1'$($Latest.URL)'"
-			"(?i)(^\s*[$]checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
-			"(?i)(^\s*[$]checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+			"(?i)(^\s*[$]packageName\s*=\s*)('.*')"    = "`$1'$($Latest.PackageName)'"
+			"(?i)(^\s*[$]url32\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
+			"(?i)(^\s*[$]checksum32\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
+			"(?i)(^\s*[$]checksumType32\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+			"(?i)(^\s*[$]url64\s*=\s*)('.*')"          = "`$1'$($Latest.URL64)'"
+			"(?i)(^\s*[$]checksum64\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum64)'"
+			"(?i)(^\s*[$]checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
 		}
 
 		"$($Latest.PackageName).nuspec" = @{
@@ -20,18 +23,20 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
 	$downloadPage = Invoke-WebRequest -Uri $releases
 
-	$re = 'ag\.zip$'
-	$url = $downloadPage.Links | Where-Object href -Match $re | Select-Object -First 1 -Expand href
+	$re = 'ag-(\d{4}-\d{2}-\d{2})_(.+)-(x64|x86)\.zip$'
+	$url = $downloadPage.Links | Where-Object href -Match $re | Select-Object -First 2 -Expand href
 
-	$version = $url -Split '/|-' | Select-Object -Last 1 -Skip 2
-	$url = 'https://github.com' + $url
-	$releaseNotes = "https://github.com/JFLarvoire/the_silver_searcher/releases/tag/${version}-Windows"
+	$version = $Matches[2] -Replace '-', '.'
+	$url32 = 'https://github.com' + $url[1] -Replace '%2F', '/'
+	$url64 = 'https://github.com' + $url[0] -Replace '%2F', '/'
+	$releaseNotes = "https://github.com/k-takata/the_silver_searcher-win32/releases/tag/$($Matches[1])/$($Matches[2])"
 
 	@{
 		Version = $version
-		URL = $url
+		URL32 = $url32
+		URL64 = $url64
 		ReleaseNotes = $releaseNotes
 	}
 }
 
-Update-Package -ChecksumFor 32
+Update-Package
