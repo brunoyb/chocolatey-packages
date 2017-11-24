19e07820eec0bbd2f4c82ac4d8c344a437155e3f
Bruno Baldochi <brunoyb@gmail.com>
2017-11-24 02:35:39 -0200

    sbt: Add package

---
+++ _personal/sbt/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName = 'sbt'
+$fileType = 'msi'
+$silentArgs = '/quiet'
+$url = 'https://github.com/sbt/sbt/releases/download/v1.0.3/sbt-1.0.3.msi'
+$checksum = '673c4a0bc029fa3fe6d1c768e1a7c60e24692e76619e506c82bb4109b1c76bff'
+$checksumType = 'sha256'
+
+Install-ChocolateyPackage -PackageName $packageName `
+                          -FileType $fileType `
+                          -SilentArgs $silentArgs `
+                          -Url $url `
+                          -Checksum $checksum `
+                          -ChecksumType $checksumType
---
+++ _personal/sbt/update.ps1
@@ -0,0 +1,37 @@
+﻿Import-Module AU
+
+$releases = 'https://github.com/sbt/sbt/releases/latest'
+
+function global:au_SearchReplace {
+	@{
+		".\tools\chocolateyInstall.ps1" = @{
+			"(?i)(^\s*[$]packageName\s*=\s*)('.*')"  = "`$1'$($Latest.PackageName)'"
+			"(?i)(^\s*[$]url\s*=\s*)('.*')"          = "`$1'$($Latest.URL)'"
+			"(?i)(^\s*[$]checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
+			"(?i)(^\s*[$]checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+		}
+
+		"$($Latest.PackageName).nuspec" = @{
+			"(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`$1$($Latest.ReleaseNotes)`$2"
+		}
+	}
+}
+
+function global:au_GetLatest {
+	$downloadPage = Invoke-WebRequest -Uri $releases
+
+	$re = 'sbt-.+\.msi$'
+	$url = $downloadPage.Links | Where-Object href -Match $re | Select-Object -First 1 -Expand href
+
+	$version = $url -Split '/|v' | Select-Object -Last 1 -Skip 1
+	$url = 'https://github.com' + $url
+	$releaseNotes = "https://github.com/sbt/sbt/releases/tag/v${version}"
+
+	@{
+		Version = $version
+		URL = $url
+		ReleaseNotes = $releaseNotes
+	}
+}
+
+Update-Package -ChecksumFor 32
