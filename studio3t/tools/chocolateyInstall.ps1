35764c93671d1531ae263ae4a6298434c9a0708d
Chocolatey <chocolatey@realdimensions.net>
2017-11-24 04:48:29 +0000

    AU: 2 updated - phraseapp studio3t [skip ci] https://gist.github.com/0c6170c4a6f7450fa864dd59684509d9/3ea6b9a16d46d5ef5a0ed8df95ebf008781d2030

--- studio3t/tools/chocolateyInstall.ps1
+++ studio3t/tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'studio3t'
-$url32 = 'https://download.studio3t.com/studio-3t/windows/5.6.3/studio-3t-x86.msi.zip'
-$checksum32 = 'fddbff1851a46f704930ee3996feeb962d0bd5070a78c344854cfd9238090b4b'
+$url32 = 'https://download.studio3t.com/studio-3t/windows/5.6.4/studio-3t-x86.msi.zip'
+$checksum32 = '27427b80f5afdabb80750aa2b660a8a30dd0a54924cc3ed7b96eb6f13e9e2618'
 $checksumType32 = 'sha256'
-$url64 = 'https://download.studio3t.com/studio-3t/windows/5.6.3/studio-3t-x64.msi.zip'
-$checksum64 = '41797b69428300cb2c06b3375c7459ddf0187b833bf58bd507e04721cd908bab'
+$url64 = 'https://download.studio3t.com/studio-3t/windows/5.6.4/studio-3t-x64.msi.zip'
+$checksum64 = '2d94b368e451f11f79de42be6b83986116f2668d29becbf9e4a2f5b809367b45'
 $checksumType64 = 'sha256'
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
