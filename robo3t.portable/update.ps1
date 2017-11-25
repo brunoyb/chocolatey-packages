68fe943ff573064bd4c715c1b7cd80a7a9072283
Bruno Baldochi <brunoyb@gmail.com>
2017-11-25 04:42:29 -0200

    robo3t.portable: Add README.md

--- robo3t.portable/update.ps1
+++ robo3t.portable/update.ps1
@@ -1,5 +1,7 @@
 ﻿Import-Module AU
 
+. $PSScriptRoot\..\_scripts\all.ps1
+
 $download = 'https://robomongo.org/download'
 
 function global:au_SearchReplace {
@@ -11,6 +13,10 @@ function global:au_SearchReplace {
 			"(?i)(^\s*[$]checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
 		}
 	}
+}
+
+function global:au_AfterUpdate {
+	Set-DescriptionFromReadme -SkipFirst 2
 }
 
 function global:au_GetLatest {
