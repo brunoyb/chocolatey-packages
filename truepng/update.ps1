91c959219437e726a73b09049c56060f8c97a810
Bruno Baldochi <brunoyb@gmail.com>
2017-11-26 17:25:45 -0200

    truepng: Add UseBasicPasing switch to iwr call
    To workaround the "Vector smash" issue.
    
    See: https://github.com/chocolatey/chocolatey-coreteampackages/issues/382

--- truepng/update.ps1
+++ truepng/update.ps1
@@ -20,7 +20,7 @@ function global:au_AfterUpdate {
 }
 
 function global:au_GetLatest {
-	$updatePage = Invoke-WebRequest -Uri $updateUrl
+	$updatePage = Invoke-WebRequest -Uri $updateUrl -UseBasicParsing
 
 	$re = 'TruePNG_\d+\.zip$'
 	$url = $updatePage.Links | Where-Object href -Match $re | Select-Object -First 1 -Expand href
