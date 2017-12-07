a9ae14b6040eace639a035763abd6eaf3035c2d4
Chocolatey <chocolatey@realdimensions.net>
2017-12-07 16:47:06 +0000

    AU: 1 updated - haskell-stack [skip ci] https://gist.github.com/0c6170c4a6f7450fa864dd59684509d9/55a46bb640a057f5a8ae238f02419810adf6f378

--- haskell-stack/tools/chocolateyInstall.ps1
+++ haskell-stack/tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'haskell-stack'
-$url32 = 'https://github.com/commercialhaskell/stack/releases/download/v1.5.1/stack-1.5.1-windows-i386.zip'
-$checksum32 = '05f91c22ea445fe07a354d39e9f1a239b6f59e652dfafdc883b368b95b88dfdd'
+$url32 = 'https://github.com/commercialhaskell/stack/releases/download/v1.6.1/stack-1.6.1-windows-i386.zip'
+$checksum32 = '0824ca4dc04546bdb711eab1a391ed553bee41a73c15a063e8424641da5b6e49'
 $checksumType32 = 'sha256'
-$url64 = 'https://github.com/commercialhaskell/stack/releases/download/v1.5.1/stack-1.5.1-windows-x86_64.zip'
-$checksum64 = 'cb6066c5620270120c6580c2331a26e840acad46acc34b44d224d66178564af6'
+$url64 = 'https://github.com/commercialhaskell/stack/releases/download/v1.6.1/stack-1.6.1-windows-x86_64.zip'
+$checksum64 = '9961a70f35c4ded26ea9e5258852b4b25fb07f03cec920f5ae649f0f5f77c2e6'
 $checksumType64 = 'sha256'
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
