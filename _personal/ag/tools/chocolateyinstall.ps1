556f92bf3ee8d91d43e5b383eb3aa708f8048765
Bruno Baldochi <brunoyb@gmail.com>
2018-01-20 23:17:03 -0200

    ag: Update to v2.1.0.22

--- _personal/ag/tools/chocolateyinstall.ps1
+++ _personal/ag/tools/chocolateyinstall.ps1
@@ -1,11 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'ag'
-$url32 = 'https://github.com/k-takata/the_silver_searcher-win32/releases/download/2017-12-19/2.1.0-11-ge2bb8d4/ag-2017-12-19_2.1.0-11-ge2bb8d4-x86.zip'
-$checksum32 = '9327cdac63ce7304e327646eba5fd7c407094981ff7fd03950de08c5e8aeb7a2'
+$url32 = 'https://github.com/k-takata/the_silver_searcher-win32/releases/download/2017-12-28/2.1.0-22-g5516568/ag-2017-12-28_2.1.0-22-g5516568-x86.zip'
+$checksum32 = '599925de0e3e5a5aeeed7e5220067f293be2db06b910353810b296fabd7c22f0'
 $checksumType32 = 'sha256'
-$url64 = 'https://github.com/k-takata/the_silver_searcher-win32/releases/download/2017-12-19/2.1.0-11-ge2bb8d4/ag-2017-12-19_2.1.0-11-ge2bb8d4-x64.zip'
-$checksum64 = 'b3b3399e8cb399ed3354144b896d9108f4eff0361888ac63005ae0aebac796f4'
+$url64 = 'https://github.com/k-takata/the_silver_searcher-win32/releases/download/2017-12-28/2.1.0-22-g5516568/ag-2017-12-28_2.1.0-22-g5516568-x64.zip'
+$checksum64 = '812d4c3e8ea29be055f91f66bdf06e5c26cbd1d90ed6b07ced096f95d91756c9'
 $checksumType64 = 'sha256'
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
