c54b59a695cb06c21bae46af5d6089118a706131
Bruno Baldochi <brunoyb@gmail.com>
2018-01-21 20:50:36 -0200

    vim: Add package

---
+++ _personal/vim/tools/chocolateyInstall.ps1
@@ -0,0 +1,24 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName = 'vim'
+$fileType = 'exe'
+$silentArgs = '/S'
+$url = 'https://github.com/vim/vim-win32-installer/releases/download/v8.0.1428/gvim_8.0.1428_x86.exe'
+$checksum = '4cc2a6a695848c63d80823d5abca0ea59854e997e97ccbd3312793df803cdeeb'
+$checksumType = 'sha256'
+
+Install-ChocolateyPackage -PackageName $packageName `
+                          -FileType $fileType `
+                          -SilentArgs $silentArgs `
+                          -Url $url `
+                          -Checksum $checksum `
+                          -ChecksumType $checksumType
+
+[array] $key = Get-UninstallRegistryKey -SoftwareName 'Vim*'
+
+if ($key.Count -eq 1) {
+	$key | ForEach-Object {
+		Write-Host 'Adding the vim installation directory to PATH …'
+		Install-ChocolateyPath "$(Split-Path -Parent $_.UninstallString)" 'Machine'
+	}
+}
---
+++ _personal/vim/update.ps1
@@ -0,0 +1,37 @@
+﻿Import-Module AU
+
+$updateUrl = 'https://github.com/vim/vim-win32-installer/releases/latest'
+
+function global:au_SearchReplace {
+	@{
+		".\tools\chocolateyInstall.ps1" = @{
+			"(?i)(^\s*[$]packageName\s*=\s*)('.*')"  = "`$1'$($Latest.PackageName)'"
+			"(?i)(^\s*[$]url\s*=\s*)('.*')"          = "`$1'$($Latest.URL)'"
+			"(?i)(^\s*[$]checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
+			"(?i)(^\s*[$]checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+		}
+
+		"$($Latest.PackageName).nuspec" = @{
+			"(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`$1$($Latest.ReleaseNotes)`$2"
+		}
+	}
+}
+
+function global:au_GetLatest {
+	$updatePage = Invoke-WebRequest -Uri $updateUrl
+
+	$re = 'gvim_.+_x86\.exe$'
+	$url = $updatePage.Links | Where-Object href -Match $re | Select-Object -First 1 -Expand href
+
+	$version = $url -Split '_' | Select-Object -Last 1 -Skip 1
+	$url = 'https://github.com' + $url
+	$releaseNotes = "https://github.com/vim/vim-win32-installer/releases/tag/v${version}"
+
+	@{
+		Version = $version
+		URL = $url
+		ReleaseNotes = $releaseNotes
+	}
+}
+
+Update-Package -ChecksumFor 32
