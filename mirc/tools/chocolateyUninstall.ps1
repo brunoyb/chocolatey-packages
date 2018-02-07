f68f58775e55a82617d82e7ab3e45be2434a63e4
Bruno Baldochi <brunoyb@gmail.com>
2018-02-07 01:14:44 -0200

    mirc: Cosmetic changes

--- mirc/tools/chocolateyUninstall.ps1
+++ mirc/tools/chocolateyUninstall.ps1
@@ -24,7 +24,7 @@ if ($key.Count -eq 1) {
 	Write-Warning "$packageName has already been uninstalled by other means."
 } elseif ($key.Count -gt 1) {
 	Write-Warning "$($key.Count) matches found!"
-	Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
-	Write-Warning "Please alert package maintainer the following keys were matched:"
+	Write-Warning 'To prevent accidental data loss, no programs will be uninstalled.'
+	Write-Warning 'Please alert package maintainer the following keys were matched:'
 	$key | ForEach-Object { Write-Warning "- $($_.DisplayName)" }
 }
