db549bce46384d374f9b447d61f7707d922c4e1a
Bruno Baldochi <brunoyb@gmail.com>
2018-04-21 00:31:52 -0300

    fossil: Add package

---
+++ _personal/fossil/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName = 'fossil'
+$url = 'https://www.fossil-scm.org/index.html/uv/fossil-w32-2.5.zip'
+$checksum = 'cad1caf56db436ca2a29e7b4919459482832dea6f6f3114258d28cbbfbc6bcb6'
+$checksumType = 'sha256'
+$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName $packageName `
+                             -Url $url `
+                             -Checksum $checksum `
+                             -ChecksumType $checksumType `
+                             -UnzipLocation $toolsDir
---
+++ _personal/fossil/update.ps1
@@ -0,0 +1,33 @@
+﻿Import-Module AU
+
+$updateUrl = 'https://www.fossil-scm.org/index.html/uv/download.html'
+
+function global:au_SearchReplace {
+	@{
+		".\tools\chocolateyInstall.ps1" = @{
+			"(?i)(^\s*[$]packageName\s*=\s*)('.*')"  = "`$1'$($Latest.PackageName)'"
+			"(?i)(^\s*[$]url\s*=\s*)('.*')"          = "`$1'$($Latest.URL)'"
+			"(?i)(^\s*[$]checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
+			"(?i)(^\s*[$]checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+		}
+	}
+}
+
+function global:au_GetLatest {
+	$updatePage = Invoke-WebRequest -Uri $updateUrl
+
+	$re = '"../timeline\?c=version-(.+)&amp;y=ci"'
+	if ($updatePage.Content -Match $re) {
+		$version = $Matches[1].Trim()
+		$url = "https://www.fossil-scm.org/index.html/uv/fossil-w32-${version}.zip"
+	} else {
+		throw "Can't match '$re'"
+	}
+
+	@{
+		Version = $version
+		URL = $url
+	}
+}
+
+Update-Package -ChecksumFor 32
