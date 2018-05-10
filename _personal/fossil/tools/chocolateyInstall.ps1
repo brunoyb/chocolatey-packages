abe822f781490e6c4c378add45c7681d15c2c330
Bruno Baldochi <brunoyb@gmail.com>
2018-05-09 23:17:51 -0300

    fossil: Update to v2.6

--- _personal/fossil/tools/chocolateyInstall.ps1
+++ _personal/fossil/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'fossil'
-$url = 'https://www.fossil-scm.org/index.html/uv/fossil-w32-2.5.zip'
-$checksum = 'cad1caf56db436ca2a29e7b4919459482832dea6f6f3114258d28cbbfbc6bcb6'
+$url = 'https://www.fossil-scm.org/index.html/uv/fossil-w32-2.6.zip'
+$checksum = '8911c87f06cfe7c9d43825e79c8331ac31723434df62dc1d53d7ba5d6ca15edc'
 $checksumType = 'sha256'
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
