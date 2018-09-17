fad2daf71446a8f91cd1302b973e4a20328b941f
Bruno Baldochi <brunoyb@gmail.com>
2018-09-17 01:53:09 -0300

    sbt: Update to v1.2.3

--- _personal/sbt/tools/chocolateyInstall.ps1
+++ _personal/sbt/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageName = 'sbt'
 $fileType = 'msi'
 $silentArgs = '/quiet'
-$url = 'https://github.com/sbt/sbt/releases/download/v1.2.1/sbt-1.2.1.msi'
-$checksum = '7f2b0450eab084dcd1104cd6ce6b6a884db5f994f237aa62bbc6598c727af174'
+$url = 'https://github.com/sbt/sbt/releases/download/v1.2.3/sbt-1.2.3.msi'
+$checksum = '006d0be2974c69e48da17e85df2cd4a389dc800980727f1bdf2609d4b8444d7e'
 $checksumType = 'sha256'
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
