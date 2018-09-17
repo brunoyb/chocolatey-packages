abc9bae0c0dd1986407acbcb7a491f7594a78604
Bruno Baldochi <brunoyb@gmail.com>
2018-09-17 01:35:02 -0300

    elixir: Update to v1.7.3

--- _personal/elixir/tools/chocolateyInstall.ps1
+++ _personal/elixir/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'elixir'
-$url = 'https://github.com/elixir-lang/elixir/releases/download/v1.7.2/Precompiled.zip'
-$checksum = 'd39c0fcdd2053f0ae2a9b1394ee582ac3398d9538881024f2252688ad92aad86'
+$url = 'https://github.com/elixir-lang/elixir/releases/download/v1.7.3/Precompiled.zip'
+$checksum = '219cc599cbb59aaece4347b1c4bb383c6e277f242ca88bc2e8a553aedaaa4c1c'
 $checksumType = 'sha256'
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
