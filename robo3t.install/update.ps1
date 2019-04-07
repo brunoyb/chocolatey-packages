9eefb3a29124f493e08695c19f27b9856d29ee05
Bruno Baldochi <brunoyb@gmail.com>
2019-04-07 16:42:06 -0300

    robo3t.install: Use GitHub to check for updates
    Most of the packages uses it and I also think it's more reliable.

--- robo3t.install/update.ps1
+++ robo3t.install/update.ps1
@@ -1,6 +1,6 @@
 ﻿Import-Module AU
 
-$updateUrl = 'https://robomongo.org/download'
+$updateUrl = 'https://github.com/Studio3T/robomongo/releases/latest'
 
 function global:au_SearchReplace {
 	@{
@@ -10,6 +10,10 @@ function global:au_SearchReplace {
 			"(?i)(^\s*[$]url64\s*=\s*)('.*')"          = "`$1'$($Latest.URL64)'"
 			"(?i)(^\s*[$]checksum64\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum64)'"
 			"(?i)(^\s*[$]checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
+		}
+
+		"$($Latest.PackageName).nuspec" = @{
+			"(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`$1$($Latest.ReleaseNotes)`$2"
 		}
 	}
 }
@@ -21,10 +25,13 @@ function global:au_GetLatest {
 	$url64 = $updatePage.Links | Where-Object href -Match $re | Select-Object -First 1 -ExpandProperty href
 
 	$version = $url64 -Split '-' | Select-Object -Last 1 -Skip 3
+	$url64 = 'https://github.com' + $url64
+	$releaseNotes = "https://github.com/Studio3T/robomongo/releases/tag/v${version}"
 
 	@{
 		Version = $version
 		URL64 = $url64
+		ReleaseNotes = $releaseNotes
 	}
 }
 
