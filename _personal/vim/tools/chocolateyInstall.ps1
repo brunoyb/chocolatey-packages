d6a2b160ad15093023775cb7ca93d7c59893df5a
Bruno Baldochi <brunoyb@gmail.com>
2019-04-30 21:33:04 -0300

    vim: Update to v8.1.1240

--- _personal/vim/tools/chocolateyInstall.ps1
+++ _personal/vim/tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'vim'
-$url32 = 'https://github.com/vim/vim-win32-installer/releases/download/v8.1.1132/gvim_8.1.1132_x86.zip'
-$checksum32 = '63601bb55decb3f1b622737711964e94ca9d81f156c32466e468f0c59171276c'
+$url32 = 'https://github.com/vim/vim-win32-installer/releases/download/v8.1.1240/gvim_8.1.1240_x86.zip'
+$checksum32 = 'c0d57bda4624543b0c2d190b70e089ded7f20d1e5327972875383253b9f15c12'
 $checksumType32 = 'sha256'
-$url64 = 'https://github.com/vim/vim-win32-installer/releases/download/v8.1.1132/gvim_8.1.1132_x64.zip'
-$checksum64 = '504ed9d255d0ab5e211bc17a4e44dca04f0a91b86579bd2e2b751825f5dc823e'
+$url64 = 'https://github.com/vim/vim-win32-installer/releases/download/v8.1.1240/gvim_8.1.1240_x64.zip'
+$checksum64 = 'd3fea30e71f8e669fce46edec0c812555d05b54fd10b8da90e7b54a59830db19'
 $checksumType64 = 'sha256'
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 $installDir = Get-ToolsLocation
