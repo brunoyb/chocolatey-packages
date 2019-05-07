fcc57d9933311db2f08fb25e31c60c50917500f4
Bruno Baldochi <brunoyb@gmail.com>
2019-05-06 23:07:32 -0300

    swi-prolog: Add missing replace instruction

--- swi-prolog/update.ps1
+++ swi-prolog/update.ps1
@@ -13,6 +13,10 @@ function global:au_SearchReplace {
 			"(?i)(^\s*[$]checksum64\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum64)'"
 			"(?i)(^\s*[$]checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
 		}
+
+		".\tools\chocolateyUninstall.ps1" = @{
+			"(?i)(^\s*[$]packageName\s*=\s*)('.*')" = "`$1'$($Latest.PackageName)'"
+		}
 	}
 }
 
