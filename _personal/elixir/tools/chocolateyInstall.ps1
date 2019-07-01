636ff17331cd981430aea718757f14c36fe58951
Bruno Baldochi <brunoyb@gmail.com>
2019-07-01 01:57:55 -0300

    elixir: Update to v1.9.0

--- _personal/elixir/tools/chocolateyInstall.ps1
+++ _personal/elixir/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'elixir'
-$url = 'https://github.com/elixir-lang/elixir/releases/download/v1.8.2/Precompiled.zip'
-$checksum = 'e16b3de37df6492c767fc25d3e921647a8e4dd0e2b3c90775baf0a7fb2547df2'
+$url = 'https://github.com/elixir-lang/elixir/releases/download/v1.9.0/Precompiled.zip'
+$checksum = '5111da32582a2d67f9a3ca8a2eb2434a5d52538fc29cd55f6e8a0ae076c8d210'
 $checksumType = 'sha256'
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
