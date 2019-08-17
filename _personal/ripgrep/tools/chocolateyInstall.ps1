3d1ae94a107659354a8d9e1b15a10c00bba0588b
Bruno Baldochi <brunoyb@gmail.com>
2019-08-17 17:59:37 -0300

    ripgrep: Update to v11.0.2

--- _personal/ripgrep/tools/chocolateyInstall.ps1
+++ _personal/ripgrep/tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'ripgrep'
-$url32 = 'https://github.com/BurntSushi/ripgrep/releases/download/11.0.1/ripgrep-11.0.1-i686-pc-windows-msvc.zip'
-$checksum32 = '07d967ffc000b74f0912a87767799eaccb008b8ab6022f7018efba9b6f681b43'
+$url32 = 'https://github.com/BurntSushi/ripgrep/releases/download/11.0.2/ripgrep-11.0.2-i686-pc-windows-msvc.zip'
+$checksum32 = '9408d9a49abc38768377378aa70c0d6615c06bbd0fd959048e1bbb7b261c3fdd'
 $checksumType32 = 'sha256'
-$url64 = 'https://github.com/BurntSushi/ripgrep/releases/download/11.0.1/ripgrep-11.0.1-x86_64-pc-windows-msvc.zip'
-$checksum64 = 'dc6fe1a859f0e27f9a8b6a637265a4b4477f8e22f4489549f295836eac53f71e'
+$url64 = 'https://github.com/BurntSushi/ripgrep/releases/download/11.0.2/ripgrep-11.0.2-x86_64-pc-windows-msvc.zip'
+$checksum64 = '0be0279591412e12b0b92d5fda0707ebeceb9c6a51472e99b46fd4d25f13b6ef'
 $checksumType64 = 'sha256'
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
