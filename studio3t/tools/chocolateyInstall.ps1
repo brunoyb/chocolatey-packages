62e9059dccea47912fa47f0cbc408eed099453b7
Chocolatey <chocolatey@realdimensions.net>
2019-09-19 16:44:33 +0000

    AU: 1 updated - studio3t [skip ci] https://gist.github.com/0c6170c4a6f7450fa864dd59684509d9/a9fae3de4fbe4296d0a37ffdfd5d8cdceafeece6

--- studio3t/tools/chocolateyInstall.ps1
+++ studio3t/tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'studio3t'
-$url32 = 'https://download.studio3t.com/studio-3t/windows/2019.5.0/studio-3t-x86-no-shell.zip'
-$checksum32 = '5c2b454f34e4277ddb7dc726fc01bcfa3a9162743c2836e60a3dac1b7314913c'
+$url32 = 'https://download.studio3t.com/studio-3t/windows/2019.5.1/studio-3t-x86-no-shell.zip'
+$checksum32 = '71a9329578d6d0c7d7755c128da889dec5e9e54fc75dc3db9a092ca56dee24dd'
 $checksumType32 = 'sha256'
-$url64 = 'https://download.studio3t.com/studio-3t/windows/2019.5.0/studio-3t-x64.zip'
-$checksum64 = '767f3617766dd0d469ba103ffb1f03628ea5909aa4dc3fe31e3d293f10164ee8'
+$url64 = 'https://download.studio3t.com/studio-3t/windows/2019.5.1/studio-3t-x64.zip'
+$checksum64 = 'ceb1ae7b7ae45c1efd5b3895c7de89d2c1f6d4d84fd4a3fad90687451ba0a317'
 $checksumType64 = 'sha256'
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
