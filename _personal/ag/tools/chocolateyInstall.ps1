c45bda6b41698ded5322ee53cafb1c4ecc971737
Bruno Baldochi <brunoyb@gmail.com>
2020-03-17 22:00:25 -0300

    ag: Update to v2.2.0.24

--- _personal/ag/tools/chocolateyInstall.ps1
+++ _personal/ag/tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'ag'
-$url32 = 'https://github.com/k-takata/the_silver_searcher-win32/releases/download/2020-01-11/2.2.0-22-ga509a81/ag-2020-01-11_2.2.0-22-ga509a81-x86.zip'
-$checksum32 = 'a2190b35d250f77270c64ac1d39c466f0953d2dcfc14398c644ae152f48663af'
+$url32 = 'https://github.com/k-takata/the_silver_searcher-win32/releases/download/2020-02-26/2.2.0-24-gb93c271/ag-2020-02-26_2.2.0-24-gb93c271-x86.zip'
+$checksum32 = '68624e20b13f2e3401c6cdc188df849047faeceba1c9b643342e4c2cc784206c'
 $checksumType32 = 'sha256'
-$url64 = 'https://github.com/k-takata/the_silver_searcher-win32/releases/download/2020-01-11/2.2.0-22-ga509a81/ag-2020-01-11_2.2.0-22-ga509a81-x64.zip'
-$checksum64 = 'aca9b259b07db3c799cf9acbe59256210fd8cdbfc60d54bf95fe1dc2cbf16495'
+$url64 = 'https://github.com/k-takata/the_silver_searcher-win32/releases/download/2020-02-26/2.2.0-24-gb93c271/ag-2020-02-26_2.2.0-24-gb93c271-x64.zip'
+$checksum64 = 'b00831313106ff6ae90691f2971afc40219361ae889303d6c3e6fac66a2ea335'
 $checksumType64 = 'sha256'
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
