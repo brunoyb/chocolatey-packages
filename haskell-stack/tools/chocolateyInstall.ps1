5534b1fe689a21ad5590567448b33c8907fe50da
Bruno Baldochi <brunoyb@gmail.com>
2020-04-30 02:12:42 -0300

    haskell-stack: Remove i386 variant
    They're not officially provided anymore.

--- haskell-stack/tools/chocolateyInstall.ps1
+++ haskell-stack/tools/chocolateyInstall.ps1
@@ -1,9 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'haskell-stack'
-$url32 = 'https://github.com/commercialhaskell/stack/releases/download/v2.1.3/stack-2.1.3-windows-i386.zip'
-$checksum32 = 'd764104ea162f12d10a5392f49587efa8f360ce179324d7e0253c74edd6f937a'
-$checksumType32 = 'sha256'
 $url64 = 'https://github.com/commercialhaskell/stack/releases/download/v2.1.3/stack-2.1.3-windows-x86_64.zip'
 $checksum64 = '415fb140c7497c4771b84e45a38b65ad47f50b9adc06499b03c4f5a8899aa32a'
 $checksumType64 = 'sha256'
@@ -12,9 +9,6 @@ $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 $packageParameters = Get-PackageParameters
 
 Install-ChocolateyZipPackage -PackageName $packageName `
-                             -Url $url32 `
-                             -Checksum $checksum32 `
-                             -ChecksumType $checksumType32 `
                              -Url64bit $url64 `
                              -Checksum64 $checksum64 `
                              -ChecksumType64 $checksumType64 `
--- haskell-stack/update.ps1
+++ haskell-stack/update.ps1
@@ -6,9 +6,6 @@ function global:au_SearchReplace {
 	@{
 		".\tools\chocolateyInstall.ps1" = @{
 			"(?i)(^\s*[$]packageName\s*=\s*)('.*')"    = "`$1'$($Latest.PackageName)'"
-			"(?i)(^\s*[$]url32\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
-			"(?i)(^\s*[$]checksum32\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
-			"(?i)(^\s*[$]checksumType32\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
 			"(?i)(^\s*[$]url64\s*=\s*)('.*')"          = "`$1'$($Latest.URL64)'"
 			"(?i)(^\s*[$]checksum64\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum64)'"
 			"(?i)(^\s*[$]checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
@@ -23,20 +20,18 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
 	$updatePage = Invoke-WebRequest -Uri $updateUrl -UseBasicParsing
 
-	$re = 'stack-.+-windows-(i386|x86_64)\.zip$'
-	$url = $updatePage.Links | Where-Object href -Match $re | Select-Object -First 2 -ExpandProperty href
+	$re = 'stack-.+-windows-x86_64\.zip$'
+	$url64 = $updatePage.Links | Where-Object href -Match $re | Select-Object -First 1 -ExpandProperty href
 
-	$version = $url[0] -Split '-' | Select-Object -Last 1 -Skip 2
-	$url32 = 'https://github.com' + $url[0]
-	$url64 = 'https://github.com' + $url[1]
+	$version = $url64 -Split '-' | Select-Object -Last 1 -Skip 2
+	$url64 = 'https://github.com' + $url64
 	$releaseNotes = "https://github.com/commercialhaskell/stack/releases/tag/v${version}"
 
 	@{
 		Version = $version
-		URL32 = $url32
 		URL64 = $url64
 		ReleaseNotes = $releaseNotes
 	}
 }
 
-Update-Package
+Update-Package -ChecksumFor 64
