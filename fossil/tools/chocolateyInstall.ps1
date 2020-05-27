edad52262419f41b329d356b182aa83c106ca85a
Bruno Baldochi <brunoyb@gmail.com>
2020-05-26 21:29:13 -0300

    fossil: Add 32 bits version

--- fossil/tools/chocolateyInstall.ps1
+++ fossil/tools/chocolateyInstall.ps1
@@ -1,12 +1,18 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'fossil'
+$url32 = 'https://www.fossil-scm.org/home/uv/fossil-w32-2.11.zip'
+$checksum32 = 'feb08809c23196fcff47cd55c6a7cfd7f005627c060a7cfc83ac7a6951393286'
+$checksumType32 = 'sha256'
 $url64 = 'https://www.fossil-scm.org/home/uv/fossil-w64-2.11.zip'
 $checksum64 = '4feb4dce04fac3f2c4446534982a1a6822e2e1aa77f1df71fdef8afe98fc52fc'
 $checksumType64 = 'sha256'
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
 Install-ChocolateyZipPackage -PackageName $packageName `
+                             -Url $url32 `
+                             -Checksum $checksum32 `
+                             -ChecksumType $checksumType32 `
                              -Url64bit $url64 `
                              -Checksum64 $checksum64 `
                              -ChecksumType64 $checksumType64 `
--- fossil/update.ps1
+++ fossil/update.ps1
@@ -6,6 +6,9 @@ function global:au_SearchReplace {
 	@{
 		".\tools\chocolateyInstall.ps1" = @{
 			"(?i)(^\s*[$]packageName\s*=\s*)('.*')"    = "`$1'$($Latest.PackageName)'"
+			"(?i)(^\s*[$]url32\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
+			"(?i)(^\s*[$]checksum32\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
+			"(?i)(^\s*[$]checksumType32\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
 			"(?i)(^\s*[$]url64\s*=\s*)('.*')"          = "`$1'$($Latest.URL64)'"
 			"(?i)(^\s*[$]checksum64\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum64)'"
 			"(?i)(^\s*[$]checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
@@ -19,6 +22,7 @@ function global:au_GetLatest {
 	$re = '\.\./timeline\?c=version-([^&]+)'
 	if ($updatePage.Content -Match $re) {
 		$version = $Matches[1].Trim()
+		$url32 = "https://www.fossil-scm.org/home/uv/fossil-w32-${version}.zip"
 		$url64 = "https://www.fossil-scm.org/home/uv/fossil-w64-${version}.zip"
 	} else {
 		throw "Can't match '$re'"
@@ -26,8 +30,9 @@ function global:au_GetLatest {
 
 	@{
 		Version = $version
+		URL32 = $url32
 		URL64 = $url64
 	}
 }
 
-Update-Package -ChecksumFor 64
+Update-Package
