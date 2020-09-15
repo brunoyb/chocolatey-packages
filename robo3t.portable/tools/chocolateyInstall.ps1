e91669a972612bda97137e50bb64e003fcc0f32e
Chocolatey <chocolatey@realdimensions.net>
2020-09-15 08:44:02 +0000

    AU: 3 updated - robo3t robo3t.install robo3t.portable [skip ci] https://gist.github.com/0c6170c4a6f7450fa864dd59684509d9/00ce8c25f6f2e4bf6c11862cf10350a58011fb49

--- robo3t.portable/tools/chocolateyInstall.ps1
+++ robo3t.portable/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'robo3t.portable'
-$url64 = 'https://github.com/Studio3T/robomongo/releases/download/v1.4.0/robo3t-1.4.0-windows-x86_64-12e54cc.zip'
-$checksum64 = 'a0d74554f0ce595f0af5c0b56466987d4d503f1e26267e683be7ebc9a974f4fd'
+$url64 = 'https://github.com/Studio3T/robomongo/releases/download/v1.4.1/robo3t-1.4.1-windows-x86_64-122dbd9.zip'
+$checksum64 = '70dee35877def8d0a6e099c39ce8428049166b09ff581ea8a469180696f31466'
 $checksumType64 = 'sha256'
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
