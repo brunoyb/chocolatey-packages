14f87e6b773abaa99291910fc888ea1efe923faa
Chocolatey <chocolatey@realdimensions.net>
2021-02-28 01:01:20 +0000

    AU: 3 updated - robo3t robo3t.install robo3t.portable [skip ci] https://gist.github.com/0c6170c4a6f7450fa864dd59684509d9/2b08fe90c6c34f0dc880fa4af1b94b0a52d6aca3

--- robo3t.install/tools/chocolateyInstall.ps1
+++ robo3t.install/tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@
 $packageName = 'robo3t.install'
 $fileType = 'exe'
 $silentArgs = '/S'
-$url64 = 'https://github.com/Studio3T/robomongo/releases/download/v1.4.3-beta/robo3t-1.4.3-windows-x86_64-48f7dfde.exe'
+$url64 = 'https://github.com/Studio3T/robomongo/releases/download/v1.4.3/robo3t-1.4.3-windows-x86_64-48f7dfde.exe'
 $checksum64 = '3253dc26ff78e5808183de541c3fbee335095ddb1662e9c7a09aede58cd2dd2b'
 $checksumType64 = 'sha256'
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
--- robo3t.portable/tools/chocolateyInstall.ps1
+++ robo3t.portable/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'robo3t.portable'
-$url64 = 'https://github.com/Studio3T/robomongo/releases/download/v1.4.3-beta/robo3t-1.4.3-windows-x86_64-48f7dfde.zip'
+$url64 = 'https://github.com/Studio3T/robomongo/releases/download/v1.4.3/robo3t-1.4.3-windows-x86_64-48f7dfde.zip'
 $checksum64 = '15c0d1d5751d68023fcca0f9e0db14dc69ea4607c2a4c9645df10b380c1653dd'
 $checksumType64 = 'sha256'
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
