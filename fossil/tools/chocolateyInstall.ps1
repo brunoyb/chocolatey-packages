8704fb5f79c24d3eb459578b28908f545e3b3d3e
Chocolatey <chocolatey@realdimensions.net>
2021-10-09 16:43:46 +0000

    AU: 1 updated - fossil [skip ci] https://gist.github.com/0c6170c4a6f7450fa864dd59684509d9/631d7d7e7f89847d7e0535c5accc8607f1675771

--- fossil/tools/chocolateyInstall.ps1
+++ fossil/tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'fossil'
-$url32 = 'https://www.fossil-scm.org/home/uv/fossil-w32-2.16.zip'
-$checksum32 = 'eaa99a2d633620a9186c71e486d95345f41189bd882004c6dbf97bb3015399ad'
+$url32 = 'https://www.fossil-scm.org/home/uv/fossil-w32-2.17.zip'
+$checksum32 = 'df38874fce63dcf93b19216c2ad720ce3232e2fc10e5111a1e5e54e4c4fead3b'
 $checksumType32 = 'sha256'
-$url64 = 'https://www.fossil-scm.org/home/uv/fossil-w64-2.16.zip'
-$checksum64 = 'c97ca3e4d3a41751f643fa68b608632a07f4c6798f34c7b938746f0405d6fd51'
+$url64 = 'https://www.fossil-scm.org/home/uv/fossil-w64-2.17.zip'
+$checksum64 = 'a2f579e26db1828cb583a079f7137c28d4884e759c27922364b0f241521627d4'
 $checksumType64 = 'sha256'
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
