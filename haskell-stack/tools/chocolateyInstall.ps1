c4a30bd7f054380a026c7e3a95679c38ad682385
Chocolatey <chocolatey@realdimensions.net>
2023-05-19 00:44:21 +0000

    AU: 1 updated - haskell-stack [skip ci] https://gist.github.com/brunoyb/0c6170c4a6f7450fa864dd59684509d9/e085d399d79d3fd462ca9c84457f22f04036accc

--- haskell-stack/tools/chocolateyInstall.ps1
+++ haskell-stack/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'haskell-stack'
-$url64 = 'https://github.com/commercialhaskell/stack/releases/download/v2.9.3/stack-2.9.3-windows-x86_64.zip'
-$checksum64 = '9403e374429aaab4d8843d9ed1bb616f7c94bea81fee0f594412b8bbb6b98970'
+$url64 = 'https://github.com/commercialhaskell/stack/releases/download/v2.11.1/stack-2.11.1-windows-x86_64.zip'
+$checksum64 = '56a9394b3be8058206aa1efa23ea40cd8a3afbfd295432de7a81d72e86615087'
 $checksumType64 = 'sha256'
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
