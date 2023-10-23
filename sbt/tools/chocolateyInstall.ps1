3cc1355ade9fbb176e808b2b54fc3b7a23746e24
Chocolatey <chocolatey@realdimensions.net>
2023-10-23 16:44:31 +0000

    AU: 1 updated - sbt [skip ci] https://gist.github.com/brunoyb/0c6170c4a6f7450fa864dd59684509d9/ace3b56d7b2434b44c9fec2ad9621749d0366314

--- sbt/tools/chocolateyInstall.ps1
+++ sbt/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageName = 'sbt'
 $fileType = 'msi'
 $silentArgs = '/quiet'
-$url = 'https://github.com/sbt/sbt/releases/download/v1.9.6/sbt-1.9.6.msi'
-$checksum = 'eefcd84174bcd1dd0a57ce72c4863048d6b1eeba701ffd2252c3bf867ca02453'
+$url = 'https://github.com/sbt/sbt/releases/download/v1.9.7/sbt-1.9.7.msi'
+$checksum = 'c941e36623615bdcfdb145d06db2ce9238610ea2e365a635f85691c403ac1cc1'
 $checksumType = 'sha256'
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
