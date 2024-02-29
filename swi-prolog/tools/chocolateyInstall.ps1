e0f61b8faa4992c6a3d65a9837df284ace62f91c
Chocolatey <chocolatey@realdimensions.net>
2024-02-29 16:45:03 +0000

    AU: 1 updated - swi-prolog [skip ci] https://gist.github.com/brunoyb/0c6170c4a6f7450fa864dd59684509d9/fff76da40be3d32fe8f34bdf1823ca77b2bf9e31

--- swi-prolog/tools/chocolateyInstall.ps1
+++ swi-prolog/tools/chocolateyInstall.ps1
@@ -3,11 +3,11 @@
 $packageName = 'swi-prolog'
 $fileType = 'exe'
 $silentArgs = '/S'
-$url32 = 'http://www.swi-prolog.org/download/stable/bin/swipl-9.2.1-1.x86.exe'
-$checksum32 = '314c4acdc31d3df309a80135b2992fc9f18f1c1d4f0263377727af1eb72a997c'
+$url32 = 'http://www.swi-prolog.org/download/stable/bin/swipl-9.2.2-1.x86.exe'
+$checksum32 = 'f4e7411888ff41846cdb8885c44deaa7200e64eeec3081891b68ac095c25319f'
 $checksumType32 = 'sha256'
-$url64 = 'http://www.swi-prolog.org/download/stable/bin/swipl-9.2.1-1.x64.exe'
-$checksum64 = 'f2122b97448291af2eece3711b32ee43ab743c50ff77a50e8151c58d2ff90ce8'
+$url64 = 'http://www.swi-prolog.org/download/stable/bin/swipl-9.2.2-1.x64.exe'
+$checksum64 = '2635a2a66842b2edf0da62cd1847e59412056a2b7f7e5583be9e6e8824a4c1b6'
 $checksumType64 = 'sha256'
 
 Install-ChocolateyPackage -PackageName $packageName `
