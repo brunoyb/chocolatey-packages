d112ecb0d23a81da0b9b2580c5e97ae872c9d15d
Chocolatey <chocolatey@realdimensions.net>
2024-07-25 16:43:56 +0000

    AU: 1 updated - mitmproxy [skip ci] https://gist.github.com/brunoyb/0c6170c4a6f7450fa864dd59684509d9/2a56d8001d2d82575651a6de5b59bfa887cfac6f

--- mitmproxy/tools/chocolateyInstall.ps1
+++ mitmproxy/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageName = 'mitmproxy'
 $fileType = 'exe'
 $silentArgs = '--mode unattended'
-$url64 = 'https://downloads.mitmproxy.org/10.3.1/mitmproxy-10.3.1-windows-x86_64-installer.exe'
-$checksum64 = 'af3bff80b0258e0f77593306882f12e0b16327f450880c5b060d91206d0f9126'
+$url64 = 'https://downloads.mitmproxy.org/10.4.0/mitmproxy-10.4.0-windows-x86_64-installer.exe'
+$checksum64 = '8d5bf83e0bf018c9b3dc7428a1c554ed02c8611a28ef8b9d093213645a4439d4'
 $checksumType64 = 'sha256'
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
