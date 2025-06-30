05d8b785b301d5252bac01911b3f461523630dc5
Chocolatey <chocolatey@realdimensions.net>
2025-06-30 16:44:35 +0000

    AU: 1 updated - studio3t [skip ci] https://gist.github.com/brunoyb/0c6170c4a6f7450fa864dd59684509d9/d8b8bd2d9cf87b4a18f9a861ceb439caf6d87d05

--- studio3t/tools/chocolateyInstall.ps1
+++ studio3t/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'studio3t'
-$url64 = 'https://download.studio3t.com/studio-3t/windows/2025.11.0/studio-3t-x64.zip'
-$checksum64 = 'e262ddf7ba645c9df554ba46c263e1f3dc521e83eb232f28108403de17c7b507'
+$url64 = 'https://download.studio3t.com/studio-3t/windows/2025.12.0/studio-3t-x64.zip'
+$checksum64 = '04f212c26b2fc70232ba527ed7e5d5cb4ae5580efc035a7669f5e53f7cc73126'
 $checksumType64 = 'sha256'
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
