85f9174b19a15dbd8484e37cff5f841437fecd51
Chocolatey <chocolatey@realdimensions.net>
2025-11-13 00:44:04 +0000

    AU: 1 updated - mirc [skip ci] https://gist.github.com/brunoyb/0c6170c4a6f7450fa864dd59684509d9/4d4c05ec7c4155e5fd17ec5dc084e186b84aae29

--- mirc/tools/chocolateyInstall.ps1
+++ mirc/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageName = 'mirc'
 $fileType = 'exe'
 $silentArgs = '/S /NOBACKUP'
-$url = 'https://www.mirc.com/get.php?version=782'
-$checksum = 'cb266beef1ba1e527d69af18859209946a7b1b9500c04900cf086a5d497fa8e1'
+$url = 'https://www.mirc.com/get.php?version=783'
+$checksum = 'c4e001b16aefb94158c6ee0324491b843639548fe5facbb04a03f45b7e3847a9'
 $checksumType = 'sha256'
 $validExitCodes = @(0, 1223)
 
